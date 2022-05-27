b7f17cea224bdabcfae9171d486bd4a845ed22df
Luan Ramos <luan.ramos@comprezipp.com>
2022-05-27 14:46:07 -0300

    criando categorias

--- micro-01/routes/api.php
+++ micro-01/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Api\{
+    CategoryController
+};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+Route::apiResource('categories', CategoryController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
